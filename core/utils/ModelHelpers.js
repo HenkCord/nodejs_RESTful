a863964b0ce5d101d2db7e0dd5c8798707e1ce88
Артём <leo240532@yandex.ru>
2016-09-05 13:48:57 +0300

    bug fix

--- core/utils/ModelHelpers.js
+++ core/utils/ModelHelpers.js
@@ -7,22 +7,30 @@ module.exports = {
      * options.offset - начало вывода записей (с 0, количество 30)
      * options.column - выводимые колонки записей (по умолчанию: *)
      * @param {Object} options
-     * @return {Object} options
      * @param {Function} cb
+     * @return {Object} options
+     *
      */
     options: function (options, cb) {
+        if(typeof options == "function"){
+            cb = options;
+            options = {};
+            options.callback = cb;
+        } else {
+            //Вызов функции
+            options.callback = typeof cb == "function" ? cb : function(){};
+        }
         //количество записей на одной странице
-        options.limit = options.limit >= 0 ? options.limit : 30;
+        options.limit = options.limit && options.limit >= 0 ? options.limit : 30;
         //страница
-        options.page = options.page >= 1 ? options.page : 1;
+        options.page = options.page && options.page >= 1 ? options.page : 1;
         //начало вывода записей (с 0, количество 30)
         options.offset = options.offset && options.offset >= 0
             ? options.offset
             : options.limit * options.page - options.limit;
         //Разрешонные для отоборажения столбцы
         options.column = options.column ? options.column : "*";
-        //Вызов функции
-        options.callback = typeof cb == "function" ? cb : function(){};
+
         return options;
     }
 };
